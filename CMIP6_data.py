f83ba0f4298c96561652ad4fc93d01a5a9c28516
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2021-02-26 14:35:13 +0100

    Update CMIP6_data.py

--- CMIP6_data.py
+++ CMIP6_data.py
@@ -142,8 +142,8 @@ else:
 	fig5 = plt.figure(figsize=(8, 0.1))
 	plot5 = (data2.tsl.isel(time=f_time-1, depth=f_depth-1)-273.15).hvplot(clim=(-50, 50), cmap='RdYlBu_r')
 	plot5
-	#st.bokeh_chart(hv.render(plot5), backend='bokeh')
-	hv.render(plot5)
+	st.bokeh_chart(hv.render(plot5), backend='bokeh')
+	#hv.render(plot5)
 	st.pyplot(fig5)
 	st.markdown('**Fig.1: Global map of Soil Temperature monthly averages ($^o$C), according to the EC-Earth3 model for the years 1850 to 1851**')
 	
