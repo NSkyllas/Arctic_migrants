eb8912052fbd1ffe127470f4c382dd7f3a10c36e
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2021-03-17 15:47:22 +0100

    Update CMIP6_data.py

--- CMIP6_data.py
+++ CMIP6_data.py
@@ -64,6 +64,7 @@ if tabletype == 'Search CMIP6 variables':
 		csv = df1.to_csv(index=False)
 		b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
 		href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+		st.markdown("## Download the table:")
 		st.markdown(href, unsafe_allow_html=True)	
 		###########################
 		
